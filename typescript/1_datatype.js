a177d7fb9ed8830183ec454b45c9a727c6c9f344
songyiping <songyiping06@163.com>
2021-01-04 11:51:50 +0800

    添加ts数据类型datatype学习文件

---
+++ typescript/1_datatype.js
@@ -0,0 +1,15 @@
+"use strict";
+var num = 5; //定义numbere类型的变量：num
+var str = "abc"; //定义string类型的变量：str
+var bool = true; //定义boolean类型的变量：boolean
+// 定义array
+var arr = ["abc", 123, true];
+var arr2 = [11, 22, 33];
+var arr3 = ['aa', 'bb', 'cc'];
+// tupple元组:他是一种类型有顺序的数组
+// 1 元组中元素的类型可以不一样
+// 2 元组中的元素的数据类型与赋值时的数据类型必须一致
+var tup = ["abc", 11, true];
+// tup[0] = "def";
+tup.push("ghi");
+// console.log(tup[3]);报错
---
+++ typescript/1_datatype.ts
@@ -0,0 +1,21 @@
+let num :number = 5;//定义numbere类型的变量：num
+
+var str :string ="abc";//定义string类型的变量：str
+
+var bool:boolean = true;//定义boolean类型的变量：boolean
+
+// 定义array
+var arr = ["abc",123,true ];
+var arr2 :number []=[11,22,33];
+var arr3: string[] = ['aa','bb','cc']
+
+// tupple元组:他是一种类型有顺序的数组
+// 1 元组中元素的类型可以不一样
+// 2 元组中的元素的数据类型与赋值时的数据类型必须一致
+var tup: [string,number,boolean] = ["abc",11,true]
+// tup[0] = "def";
+tup.push("ghi")
+
+// console.log(tup[3]);报错
+
+
